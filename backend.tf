c2a2e4ab64de5fcaf259bf8da5c6622ee8cd8e94
pradyumna reddy <60683250+kalvapradyumnareddy@users.noreply.github.com>
2024-02-01 09:57:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@ terraform{
 backend "azurerm"{
 
   resource_group_name    =  "kalva"
-  storage_account_name   =  "Slytherin"
-  container              =  "Malfoy"
+  storage_account_name   =  "slytherin"
+  container              =  "malfoy"
   key                    =  "terraform.tfstate"
 
 
